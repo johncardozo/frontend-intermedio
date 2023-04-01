4c07ae83aafd329a3b974ef1a77e6bf4b1b82c1d
John Cardozo <johncardozo@gmail.com>
2023-03-31 19:58:43 -0500

    JS: Operadores matemáticos

---
+++ javascript/js/opMat.js
@@ -0,0 +1,43 @@
+// Comentario de una linea
+/* Comentario de 
+varias 
+lineas */
+
+// Suma de constantes
+let suma = 5 + 4;
+console.log(suma);
+
+// suma de constantes y variables
+let resSuma = suma + 10;
+console.log(resSuma);
+
+// No modifica la variable
+console.log(resSuma + 20);
+
+// Shortcuts de incremento/decremento
+resSuma++;
+console.log(resSuma);
+resSuma--;
+console.log(resSuma);
+resSuma += 6;
+console.log(resSuma);
+resSuma -= 10;
+console.log(resSuma);
+
+// Operador de resta
+let resta = resSuma - 10;
+console.log(resta);
+
+// Multiplicación
+let multiplicacion = resta * 2;
+console.log(multiplicacion);
+
+// División
+let division = multiplicacion / 3;
+console.log(division);
+
+// Residuo (módulo)
+let residuo = 20 % 3; // Sobra 2
+division = 20 / 3; // 6.66666
+console.log(residuo);
+console.log(division);
