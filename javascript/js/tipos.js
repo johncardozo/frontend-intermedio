a8231e1197b8a382cabb8e163744aaef0d3d920d
John Cardozo <johncardozo@gmail.com>
2023-03-31 19:40:49 -0500

    JS: tipos de datos

---
+++ javascript/js/tipos.js
@@ -0,0 +1,27 @@
+// String
+let cadena = "Distrital";
+console.log(cadena);
+console.log(typeof cadena);
+
+// Number
+let entero = 21;
+console.log(entero);
+console.log(typeof entero);
+let real = 14.56;
+console.log(real);
+console.log(typeof real);
+
+// Boolean -> true | false
+let valor = false;
+console.log(valor);
+console.log(typeof valor);
+
+// undefined
+let noDefinido;
+console.log(noDefinido);
+console.log(typeof noDefinido);
+
+// null -> object
+let ninguno = null;
+console.log(ninguno);
+console.log(typeof ninguno);
---
+++ javascript/js/variables.js
@@ -0,0 +1,31 @@
+// Declaración de variables (deprecated)
+var nombre = "Catalina";
+
+// Variables ECMAScript6
+let curso = "Frontend Intermedio";
+console.log(curso);
+
+// Constantes ECMAScript6
+const PI = 3.1416;
+console.log(PI);
+
+// Case sentitive
+let universidad = "Distrital";
+let uNiVeRsIdAd = "Nacional";
+let universidaD = "Pedagogica";
+console.log(universidad);
+console.log(uNiVeRsIdAd);
+console.log(universidaD);
+
+let edad = 21;
+console.log(edad);
+
+edad = 25;
+console.log(edad);
+
+const mes = 1;
+console.log(mes);
+
+// genera un error porque no se puede
+// cambiar el valor de una constante
+//mes = 2;
