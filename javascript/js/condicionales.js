8011ff6f5ad376da5d1dc182d047dfb417646365
John Cardozo <johncardozo@gmail.com>
2023-04-03 20:00:30 -0500

    JS: Expresiones booleanas

---
+++ javascript/js/condicionales.js
@@ -0,0 +1,23 @@
+console.log(5 > 5);
+console.log(5 >= 5);
+console.log(5 < 5);
+console.log(5 <= 5);
+// Comparación de valor -> ==
+console.log(5 == 5);
+console.log(5 == "5"); // true
+console.log(0 == false); // true
+// Comparación de valor y tipo -> ===
+console.log(5 === 5);
+console.log(5 === "5");
+console.log(5 != "5");
+console.log(5 !== "5");
+// Negación
+console.log(!(5 === 5));
+
+let a = 5;
+let b = 6;
+
+console.log(a > 4 && a < 10); // true
+console.log(a > 3 && a < 2); // false
+console.log(a > 4 || a < 2); // true
+console.log(a > 6 || a < 3); // false
