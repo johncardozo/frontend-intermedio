76af630d2003a12a84f1b2cdbda3df637df42c87
John Cardozo <johncardozo@gmail.com>
2023-04-11 18:29:37 -0500

    JS: módulos

---
+++ javascript/js/modulos/app.js
@@ -0,0 +1,9 @@
+import { sumar, restar, MESES } from "./libreria.js";
+
+let resultado = sumar(20, 30);
+console.log(resultado);
+
+resultado = restar(40, 39);
+console.log(resultado);
+
+console.log(MESES);
---
+++ javascript/js/modulos/libreria.js
@@ -0,0 +1,16 @@
+/**
+ * Calcula la suma de 2 valores numéricos
+ * @param {*} numero1 Primer valor
+ * @param {*} numero2 Segundo valor
+ * @returns Sumatoria de los valores recibidos
+ */
+const sumar = (numero1, numero2) => {
+  return numero1 + numero2;
+};
+const restar = (numero1, numero2) => {
+  return numero1 - numero2;
+};
+
+const MESES = 12;
+
+export { sumar, restar, MESES };
