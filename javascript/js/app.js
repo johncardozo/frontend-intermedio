b3b4e970233ae36495e28be0621d3472139aff10
John Cardozo <johncardozo@gmail.com>
2023-04-13 18:32:50 -0500

    DOM: Modificar estilos y clases CSS con Javascript

--- javascript/js/app.js
+++ javascript/js/app.js
@@ -16,3 +16,19 @@ const srcImg1 = img1.getAttribute("src");
 console.log(srcImg1);
 // Modifica el atributo de un elemento
 img2.setAttribute("src", srcImg1);
+
+// Obtiene la lista
+const lista = document.getElementById("lista");
+console.log(lista);
+// Cambiar el estilo
+lista.style.color = "orange";
+// camelCase
+lista.style.backgroundColor = "green";
+
+// Obtiene el botón
+const boton = document.getElementById("boton");
+// Agrega una clase CSS al elemento
+boton.classList.add("btn");
+boton.classList.add("border");
+// Elimina una clase CSS del elemento
+boton.classList.remove("btn");
