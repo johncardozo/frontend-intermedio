d27a7fa71826842887332b1ba7c2efb09726412e
John Cardozo <johncardozo@gmail.com>
2023-04-14 18:42:41 -0500

    DOM: Navegación por el DOM Tree (padres, hijos)

--- javascript/js/app.js
+++ javascript/js/app.js
@@ -1,30 +1,50 @@
-const boton = document.getElementById("boton");
-const indicador = document.getElementById("indicador");
-const mouseClickHandler = () => {
-  indicador.classList.add("click");
-  indicador.classList.remove("over");
-  indicador.classList.remove("out");
-};
-const mouseOverHandler = () => {
-  indicador.classList.add("over");
-  indicador.classList.remove("click");
-  indicador.classList.remove("out");
-};
-const mouseOutHandler = () => {
-  indicador.classList.add("out");
-  indicador.classList.remove("over");
-  indicador.classList.remove("click");
-};
-boton.addEventListener("click", mouseClickHandler);
-boton.addEventListener("mouseover", mouseOverHandler);
-boton.addEventListener("mouseout", mouseOutHandler);
+// Obtiene referencia al elemento
+const area = document.getElementById("area");
 
-const linkMenu = document.getElementById("linkMenu");
-const menu = document.getElementById("menu");
+// Obtiene los hijos del elemento
+const hijos = area.children;
+console.log(hijos);
 
-linkMenu.addEventListener("mouseover", () => {
-  menu.style.display = "block";
-});
-linkMenu.addEventListener("mouseout", () => {
-  menu.style.display = "none";
-});
+// Recorre la lista de hijos
+for (const hijo of hijos) {
+  console.log(hijo.textContent);
+}
+console.log(`La cantidad de hijos del div es ${hijos.length}`);
+
+// Obtiene el primer hijo
+const primerHijo = area.firstElementChild;
+console.log(primerHijo);
+
+// Obtiene el último hijo
+const ultimoHijo = area.lastElementChild;
+console.log(ultimoHijo);
+
+// Obtiene un hijo dada la posición
+const algunHijo = hijos[1];
+console.log(algunHijo);
+console.log(area.children[2]);
+
+// Obtiene la referencia a los elementos
+const titulo = document.getElementById("titulo");
+const parrafo = document.getElementById("parrafo");
+
+console.log(titulo);
+console.log(parrafo);
+
+// Obtiene el padre de un elemento
+const padre = titulo.parentElement;
+console.log(padre);
+
+// Obtiene el ultimo hijo del padre
+console.log(padre.lastElementChild);
+
+// Chaining invocación de propiedades
+console.log(parrafo.parentElement.lastElementChild);
+
+// Obtiene el articulo
+const articulo = document.querySelector("article");
+console.log(articulo);
+
+// Obtiene el padre del padre (chaining)
+const abuelo = articulo.parentElement.parentElement;
+console.log(abuelo);
