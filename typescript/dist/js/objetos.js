73662faf6ce6b89c030bf2b1c87dc76b70f01663
John Cardozo <johncardozo@gmail.com>
2023-04-25 19:05:41 -0500

    TS: Sobrecarga de constructores

--- typescript/dist/js/objetos.js
+++ typescript/dist/js/objetos.js
@@ -1,15 +1,20 @@
 "use strict";
 class Training {
-    constructor() {
-        this.type = "";
-        this.distance = 0;
-        this.date = "";
+    constructor(type = "Training run", distance = 0, date = "today") {
+        this.type = type;
+        this.distance = distance;
+        this.date = date;
     }
 }
-const t1 = new Training();
-const t2 = new Training();
-t1.distance = 10;
-t2.distance = 21;
+const t1 = new Training("Long run", 21, "2023-04-20");
+const t2 = new Training("Intervals", 8, "2023-04-21");
+const t3 = new Training("Long run", 10, "2023-04-22");
+const t4 = new Training("Long run", 10);
+const t5 = new Training("Intervals");
+const t6 = new Training();
 console.log(t1);
 console.log(t2);
-console.log(t1.distance);
+console.log(t3);
+console.log(t4);
+console.log(t5);
+console.log(t6);
--- typescript/src/ts/objetos.ts
+++ typescript/src/ts/objetos.ts
@@ -1,19 +1,34 @@
 // Definición de una clase
 class Training {
-  type: string = "";
-  distance: number = 0;
-  date: string = "";
-}
+  type: string;
+  distance: number;
+  date: string;
 
-// Crea o instancia un nuevo objeto
-const t1 = new Training();
-const t2 = new Training();
-// Modifica las propiedades de un objeto
-t1.distance = 10;
-t2.distance = 21;
+  // type, distance, date
+  // type, distance
+  // type
+  // sin parámetros
+  constructor(
+    type: string = "Training run",
+    distance: number = 0,
+    date: string = "today"
+  ) {
+    this.type = type;
+    this.distance = distance;
+    this.date = date;
+  }
+}
+// Sobrecarga de constructores
+const t1 = new Training("Long run", 21, "2023-04-20");
+const t2 = new Training("Intervals", 8, "2023-04-21");
+const t3 = new Training("Long run", 10, "2023-04-22");
+const t4 = new Training("Long run", 10);
+const t5 = new Training("Intervals");
+const t6 = new Training();
 
 console.log(t1);
 console.log(t2);
-
-// Obtiene el valor de una propiedad de un objeto
-console.log(t1.distance);
+console.log(t3);
+console.log(t4);
+console.log(t5);
+console.log(t6);
