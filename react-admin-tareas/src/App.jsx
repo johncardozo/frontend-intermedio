9770163b6f474f2866bd58954148c1886558e1ff
John Cardozo <johncardozo@gmail.com>
2023-05-11 19:44:36 -0500

    React: propiedades de componentes

--- react-admin-tareas/src/App.jsx
+++ react-admin-tareas/src/App.jsx
@@ -1,7 +1,15 @@
 import Header from "./components/Header";
+import Opcion from "./components/Opcion";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Opcion texto="Home" />
+      <Opcion texto="Products" />
+      <Opcion texto="Services" />
+    </>
+  );
 }
 
 export default App;
---
+++ react-admin-tareas/src/components/Opcion.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+
+const Opcion = (props) => {
+  return <div>{props.texto}</div>;
+};
+
+export default Opcion;
