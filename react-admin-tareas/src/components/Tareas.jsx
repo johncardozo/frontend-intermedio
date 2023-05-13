c59ea65f24afe210e650900656af034968292e3e
John Cardozo <johncardozo@gmail.com>
2023-05-12 19:07:40 -0500

    React: renderizado condicional

--- react-admin-tareas/src/components/Tareas.jsx
+++ react-admin-tareas/src/components/Tareas.jsx
@@ -4,15 +4,21 @@ const Tareas = () => {
     { id: 1, titulo: "Running" },
     { id: 2, titulo: "Programming" },
     { id: 3, titulo: "Reading" },
-    { id: 4, titulo: "Swimming" },
   ];
 
   return (
-    <ul>
-      {listaTareas.map((tarea) => (
-        <li key={tarea.id}>{tarea.titulo}</li>
-      ))}
-    </ul>
+    <>
+      {listaTareas.length > 0 ? (
+        <h2>Hay {listaTareas.length} tareas</h2>
+      ) : (
+        <h2>No hay tareas</h2>
+      )}
+      <ul>
+        {listaTareas.map((tarea) => (
+          <li key={tarea.id}>{tarea.titulo}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
