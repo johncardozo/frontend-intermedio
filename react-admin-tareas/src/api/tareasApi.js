a2513b5437f61e8d1d6cc31e88c72fbd736aa889
John Cardozo <johncardozo@gmail.com>
2023-05-19 19:58:03 -0500

    React: Uso de variables de entorno en Vite

--- react-admin-tareas/src/api/tareasApi.js
+++ react-admin-tareas/src/api/tareasApi.js
@@ -7,7 +7,7 @@ import axios from "axios";
  */
 const obtenerTareasAPI = async () => {
   try {
-    const respuesta = await axios.get("http://localhost:3000/tareas/");
+    const respuesta = await axios.get(import.meta.env.VITE_BACKEND_URL);
     if (respuesta.status === 200) {
       return respuesta.data;
     }
@@ -24,7 +24,7 @@ const obtenerTareasAPI = async () => {
  */
 const agregarTareaAPI = async (tarea) => {
   try {
-    const respuesta = await axios.post("http://localhost:3000/tareas/", tarea);
+    const respuesta = await axios.post(import.meta.env.VITE_BACKEND_URL, tarea);
     if (respuesta.status === 201) {
       return respuesta.data;
     }
