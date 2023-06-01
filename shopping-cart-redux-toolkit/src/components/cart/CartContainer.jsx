d08b8dfd033b8d055b9c28f4926e42b971f27eef
John Cardozo <johncardozo@gmail.com>
2023-05-31 19:25:33 -0500

    Redux Toolkit: Componente CartEmpty

--- shopping-cart-redux-toolkit/src/components/cart/CartContainer.jsx
+++ shopping-cart-redux-toolkit/src/components/cart/CartContainer.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import CartFooter from "./CartFooter";
+import CartEmpty from "./CartEmpty";
 
 const CartContainer = () => {
   // Obtiene acceso al store
@@ -9,6 +10,7 @@ const CartContainer = () => {
   return (
     <section className="cart-container">
       <header>Your Shopping Cart</header>
+      {!cartItems.length && <CartEmpty />}
       <div>
         {cartItems.map((item) => {
           return <CartItem key={item.id} {...item} />;
---
+++ shopping-cart-redux-toolkit/src/components/cart/CartEmpty.jsx
@@ -0,0 +1,9 @@
+const CartEmpty = () => {
+  return (
+    <div className="cart-empty">
+      <h3>😔 Shopping cart empty</h3>
+    </div>
+  );
+};
+
+export default CartEmpty;
