26607de0a0e1ff53355cc676bc9873f6494e7b2f
John Cardozo <johncardozo@gmail.com>
2023-06-01 19:15:13 -0500

    Redux Toolkit: Limpia el cart desde el modal y lo cierra

--- shopping-cart-redux-toolkit/src/components/Modal.jsx
+++ shopping-cart-redux-toolkit/src/components/Modal.jsx
@@ -1,13 +1,31 @@
+import { useDispatch } from "react-redux";
+import { closeModal } from "../features/modal/modalSlice";
+import { clearCart } from "../features/cart/cartSlice";
+
 const Modal = () => {
+  const dispatch = useDispatch();
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>Are you sure you want to clear the cart?</h4>
         <div className="btn-container">
-          <button type="button" className="btn btn-confirm">
+          <button
+            type="button"
+            className="btn btn-confirm"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
             Confirm
           </button>
-          <button type="button" className="btn btn-cancel">
+          <button
+            type="button"
+            className="btn btn-cancel"
+            onClick={() => {
+              dispatch(closeModal());
+            }}
+          >
             Cancel
           </button>
         </div>
--- shopping-cart-redux-toolkit/src/features/modal/modalSlice.js
+++ shopping-cart-redux-toolkit/src/features/modal/modalSlice.js
@@ -13,11 +13,14 @@ const modalSlice = createSlice({
     openModal: (state) => {
       state.isOpen = true;
     },
+    closeModal: (state) => {
+      state.isOpen = false;
+    },
   },
 });
 
 // Exporta los reducers
-export const { openModal } = modalSlice.actions;
+export const { openModal, closeModal } = modalSlice.actions;
 
 // Exporta el Slice con reducer
 export default modalSlice.reducer;
