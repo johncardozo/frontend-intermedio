ae07f70912b190ebc0696578feb13a334ffc79c1
John Cardozo <johncardozo@gmail.com>
2023-06-21 19:05:52 -0500

    Storybook: Componente Botón con argumento Size

--- storybook-react/src/stories/Boton/Boton.jsx
+++ storybook-react/src/stories/Boton/Boton.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import "./Boton.css";
 
-export const Boton = ({ label, primary }) => {
+export const Boton = ({ label, primary, size }) => {
   const tipo = primary ? "btn-primary" : "btn-secondary";
   return (
-    <button type="button" className={`btn ${tipo}`}>
+    <button type="button" className={`btn ${tipo} btn-${size}`}>
       {label}
     </button>
   );
@@ -13,9 +13,11 @@ export const Boton = ({ label, primary }) => {
 Boton.propTypes = {
   label: PropTypes.string,
   primary: PropTypes.bool,
+  size: PropTypes.oneOf(["small", "medium", "large"]),
 };
 
 Boton.defaultProps = {
   label: "Botón",
   primary: true,
+  size: "medium",
 };
--- storybook-react/src/stories/Boton/Boton.stories.js
+++ storybook-react/src/stories/Boton/Boton.stories.js
@@ -17,3 +17,24 @@ export const Secondary = {
     primary: false,
   },
 };
+export const Large = {
+  args: {
+    label: "Grande",
+    primary: false,
+    size: "large",
+  },
+};
+export const Medium = {
+  args: {
+    label: "Mediano",
+    primary: false,
+    size: "medium",
+  },
+};
+export const Small = {
+  args: {
+    label: "Pequeñito",
+    primary: true,
+    size: "small",
+  },
+};
