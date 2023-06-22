ff5a4c9f5fd6b9f74b5dc201449d7a80bd8d097b
John Cardozo <johncardozo@gmail.com>
2023-06-22 18:20:56 -0500

    Storybook: Componente Titulo con parámetros backgroundColor y textColor

--- storybook-react/src/stories/Titulo/Titulo.jsx
+++ storybook-react/src/stories/Titulo/Titulo.jsx
@@ -1,13 +1,17 @@
 import PropTypes from "prop-types";
 import "./Titulo.css";
 
-const Titulo = ({ texto, nivel }) => {
-  if (nivel === 1) return <h1>{texto}</h1>;
-  if (nivel === 2) return <h2>{texto}</h2>;
-  if (nivel === 3) return <h3>{texto}</h3>;
-  if (nivel === 4) return <h4>{texto}</h4>;
-  if (nivel === 5) return <h5>{texto}</h5>;
-  if (nivel === 6) return <h6>{texto}</h6>;
+const Titulo = ({ texto, nivel, backgroundColor, textColor }) => {
+  const estilo = {
+    backgroundColor,
+    color: textColor,
+  };
+  if (nivel === 1) return <h1 style={estilo}>{texto}</h1>;
+  if (nivel === 2) return <h2 style={estilo}>{texto}</h2>;
+  if (nivel === 3) return <h3 style={estilo}>{texto}</h3>;
+  if (nivel === 4) return <h4 style={estilo}>{texto}</h4>;
+  if (nivel === 5) return <h5 style={estilo}>{texto}</h5>;
+  if (nivel === 6) return <h6 style={estilo}>{texto}</h6>;
 
   return <h1>{texto}</h1>;
 };
@@ -15,11 +19,15 @@ const Titulo = ({ texto, nivel }) => {
 Titulo.propTypes = {
   texto: PropTypes.string,
   nivel: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
 };
 
 Titulo.defaultProps = {
   texto: "título",
   nivel: 1,
+  backgroundColor: "#fff",
+  textColor: "#000",
 };
 
 export default Titulo;
--- storybook-react/src/stories/Titulo/Titulo.stories.js
+++ storybook-react/src/stories/Titulo/Titulo.stories.js
@@ -15,17 +15,23 @@ export const Header1 = {
   args: {
     texto: "Header 1",
     nivel: 1,
+    backgroundColor: "yellow",
+    textColor: "#000",
   },
 };
 export const Header3 = {
   args: {
     texto: "Header 3",
     nivel: 3,
+    backgroundColor: "blue",
+    textColor: "white",
   },
 };
 export const Header6 = {
   args: {
     texto: "Header 6",
     nivel: 6,
+    backgroundColor: "red",
+    textColor: "white",
   },
 };
