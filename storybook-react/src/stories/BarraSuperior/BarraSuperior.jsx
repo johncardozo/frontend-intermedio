79bcdb2f92c16f64c5847ccf63c0852971261308
John Cardozo <johncardozo@gmail.com>
2023-06-22 19:20:08 -0500

    Storybook: Componente Molécula: Barra Superior

--- storybook-react/src/stories/BarraSuperior/BarraSuperior.jsx
+++ storybook-react/src/stories/BarraSuperior/BarraSuperior.jsx
@@ -1,5 +1,19 @@
+import Titulo from "../Titulo/Titulo";
+import { Boton } from "../Boton/Boton";
+import "./BarraSuperior.css";
+
 const BarraSuperior = () => {
-  return <div>BarraSuperior</div>;
+  return (
+    <div className="toolbar">
+      <Titulo texto="Frontend Inc." nivel={2} textColor="#019CFD" />
+      <div className="toolbar-btns">
+        <Boton label="inicio" />
+        <Boton label="productos" />
+        <Boton label="servicios" />
+        <Boton label="ingresar" primary={false} />
+      </div>
+    </div>
+  );
 };
 
 export default BarraSuperior;
