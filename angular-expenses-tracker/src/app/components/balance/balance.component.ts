e3337db33d5ff2a74c3bb23e8ecaa28c396e63bd
John Cardozo <johncardozo@gmail.com>
2023-06-27 18:52:48 -0500

    Angular: Datos complejos (interfaces) como parámetros de componentes

--- angular-expenses-tracker/src/app/components/balance/balance.component.ts
+++ angular-expenses-tracker/src/app/components/balance/balance.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { Balance } from '../../models/balance.model';
 @Component({
   selector: 'app-balance',
   templateUrl: './balance.component.html',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class BalanceComponent {
   @Input() title: string = '';
+  @Input() balance: Balance = {
+    amount: 55_000,
+    income: 100_00,
+    expenses: 45_000,
+  };
 }
