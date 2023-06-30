d087c3b3ce526bb73498e8f433e6ffd7607f269c
John Cardozo <johncardozo@gmail.com>
2023-06-29 19:41:47 -0500

    Angular: ngStyle para la fecha del componente Transaction

--- angular-expenses-tracker/src/app/components/transaction/transaction.component.ts
+++ angular-expenses-tracker/src/app/components/transaction/transaction.component.ts
@@ -8,4 +8,12 @@ import { Transaction } from '../../models/transaction.model';
 })
 export class TransactionComponent {
   @Input() transaction!: Transaction;
+
+  dateStyles: Record<string, string> = {
+    'border-bottom':
+      this.transaction?.type === 'expense'
+        ? '1px solid red'
+        : '1px solid green',
+    'margin-top': '3px',
+  };
 }
