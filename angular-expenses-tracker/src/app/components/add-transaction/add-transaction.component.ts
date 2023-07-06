96a1033f07f820b9e1750eef40522ccc882d45bc
John Cardozo <johncardozo@gmail.com>
2023-07-05 19:04:09 -0500

    Angular: Validaciones de formulario, muestra/oculta mensajes de error

--- angular-expenses-tracker/src/app/components/add-transaction/add-transaction.component.ts
+++ angular-expenses-tracker/src/app/components/add-transaction/add-transaction.component.ts
@@ -1,6 +1,6 @@
 import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-transaction',
@@ -15,10 +15,20 @@ export class AddTransactionComponent implements OnInit {
     const date: string = formatDate(new Date(2022, 7, 30), 'yyyy-MM-dd', 'en');
 
     this.addTransactionForm = new FormGroup({
-      amount: new FormControl(0),
+      amount: new FormControl(120, Validators.required),
       type: new FormControl('expense'),
       category: new FormControl('food'),
       date: new FormControl(date),
     });
+  }
+
+  onSubmit(): void {
+    if (this.addTransactionForm.valid) {
+      console.log(this.addTransactionForm);
+      console.log(this.addTransactionForm.value);
+      alert("Everything's ok");
+    } else {
+      console.error('The form is not valid');
+    }
   }
 }
