641561921593c8978db2eb7416945f9dab2e8aa2
John Cardozo <johncardozo@gmail.com>
2023-07-11 18:58:12 -0500

    Angular: Unit Testing - Prueba a componente Balance

--- angular-expenses-tracker/src/app/components/balance/balance.component.spec.ts
+++ angular-expenses-tracker/src/app/components/balance/balance.component.spec.ts
@@ -5,17 +5,48 @@ import { BalanceComponent } from './balance.component';
 describe('BalanceComponent', () => {
   let component: BalanceComponent;
   let fixture: ComponentFixture<BalanceComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BalanceComponent]
+      declarations: [BalanceComponent],
     });
     fixture = TestBed.createComponent(BalanceComponent);
     component = fixture.componentInstance;
+    // Monta y recarga el componente
     fixture.detectChanges();
+    // Obtener el template
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the right amount, income and expenses', () => {
+    // Establece el valor de una propiedad del componente
+    component.balance = {
+      amount: 100,
+      income: 150,
+      expenses: 50,
+    };
+    // Monta y recarga el componente
+    fixture.detectChanges();
+
+    // Ejecuta la prueba para verificar el valor de amount
+    const amountElement = compiled.querySelector('h2');
+    expect(amountElement?.textContent).toBe('$100.00');
+
+    // Ejecuta la prueba para verificar el valor de income
+    const incomeElement = compiled.querySelector(
+      '.balance__info div:nth-child(1) h3'
+    );
+    expect(incomeElement?.textContent).toBe('$150.00');
+
+    // Ejecuta la prueba para verificar el valor de expenses
+    const expensesElement = compiled.querySelector(
+      '.balance__info div:nth-child(2) h3'
+    );
+    expect(expensesElement?.textContent).toBe('$50.00');
   });
 });
