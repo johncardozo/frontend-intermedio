e1bcacdec153417cebb646260468b129b3d4c39d
John Cardozo <johncardozo@gmail.com>
2023-07-12 18:33:17 -0500

    Angular: Unit testing - Creación de pruebas de un servicio asícncrono

--- angular-expenses-tracker/src/app/services/transactions.service.spec.ts
+++ angular-expenses-tracker/src/app/services/transactions.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TransactionsService } from './transactions.service';
+import { Transaction } from '../models/transaction.model';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(TransactionsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all transactions', (done) => {
+    service.get().subscribe((transactions: Transaction[]) => {
+      expect(transactions.length).toBe(6);
+      done();
+    });
   });
 });
