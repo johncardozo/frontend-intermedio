be8751cf2a974833eabba70a20490fad00640da5
John Cardozo <johncardozo@gmail.com>
2023-07-12 19:39:55 -0500

    Angular: Unit testing - Soluciona los errores de dependencias del componente HomeComponent

--- angular-expenses-tracker/src/app/components/home/home.component.spec.ts
+++ angular-expenses-tracker/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
+import { BalanceComponent } from '../balance/balance.component';
 import { HomeComponent } from './home.component';
+import { TransactionsComponent } from '../transactions/transactions.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +11,8 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      imports: [HttpClientModule],
+      declarations: [HomeComponent, BalanceComponent, TransactionsComponent],
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
