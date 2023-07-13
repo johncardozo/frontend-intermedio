91e22ecaf2424f750e80ca215752d233b593a0f7
John Cardozo <johncardozo@gmail.com>
2023-07-12 19:58:35 -0500

    Angular: Unit testing - Crea un test para probar la función calculateBalance de HomeComponent

--- angular-expenses-tracker/src/app/components/home/home.component.spec.ts
+++ angular-expenses-tracker/src/app/components/home/home.component.spec.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BalanceComponent } from '../balance/balance.component';
 import { HomeComponent } from './home.component';
 import { TransactionsComponent } from '../transactions/transactions.component';
+import { Transaction } from '../../models/transaction.model';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -28,5 +29,37 @@ describe('HomeComponent', () => {
   it('should have a div with css class main', () => {
     const div = compiled.querySelector('.main');
     expect(div).toBeTruthy();
+  });
+
+  it('should calculate the balance based on transactions', () => {
+    const dummyTransactions: Transaction[] = [
+      {
+        id: '1',
+        type: 'income',
+        amount: 500,
+        category: 'payroll',
+        date: new Date(),
+      },
+      {
+        id: '2',
+        type: 'expense',
+        amount: 100,
+        category: 'entertainment',
+        date: new Date(),
+      },
+      {
+        id: '3',
+        type: 'expense',
+        amount: 200,
+        category: 'food',
+        date: new Date(),
+      },
+    ];
+
+    component.transactions = dummyTransactions;
+    component.calculateBalance();
+    expect(component.balance.income).toBe(500);
+    expect(component.balance.expenses).toBe(300);
+    expect(component.balance.amount).toBe(200);
   });
 });
