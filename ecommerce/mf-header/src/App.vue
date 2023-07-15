13f14f4c476162985ff804f5519eb8dcd35ca9e8
John Cardozo <johncardozo@gmail.com>
2023-07-14 19:31:13 -0500

    Microfrontend: Agrega la lógica y estilos de la aplicación Vue mf-header

--- ecommerce/mf-header/src/App.vue
+++ ecommerce/mf-header/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "./components/HeaderComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderComponent,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ ecommerce/mf-header/src/components/HeaderComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <h2>Atenea Ecommerce</h2>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Deals</a></li>
+      <li><a href="#">Cart</a></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+header {
+  padding: 10px 40px;
+  background-image: linear-gradient(
+    to right,
+    #103061,
+    #174d84,
+    #196ba8,
+    #158ccc,
+    #07aef0
+  );
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header h2 {
+  color: #fff;
+  margin: 0;
+  padding: 0;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 400;
+}
+header ul {
+  list-style-type: none;
+  display: flex;
+  gap: 20px;
+}
+header a {
+  font-family: "Montserrat", sans-serif;
+  color: #fff;
+  text-decoration: none;
+}
+</style>
